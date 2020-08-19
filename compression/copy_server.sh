71b6bced1ee8a814cca671b769b3b0f52a1de5cd
lucasgneccoh <lucasgneccoh@gmail.com>
2020-08-19 13:15:20 +0200

    tests on compression/decompresson results

--- compression/copy_server.sh
+++ compression/copy_server.sh
@@ -1,6 +1,6 @@
 #From build dir
-from="/home/lucas/Documents/stage/gedlib/compression/"
-to="lgnecco@ssh.lamsade.dauphine.fr:/home/lamsade/lgnecco/stage/gedlib/compression"
+from="lgnecco@ssh.lamsade.dauphine.fr:/home/lamsade/lgnecco/stage/gedlib/compression/data/output/compressed/"
+to="/home/lucas/Documents/stage/gedlib/compression/data/from_server"
 #from="/home/lucas/Documents/stage/gedlib/compression/src/"
 #to="lgnecco@ssh.lamsade.dauphine.fr:/home/lamsade/lgnecco/stage/gedlib/compression/src"
 echo -ne '\007'
--- compression/data/from_server/get_server.sh
+++
@@ -1,8 +0,0 @@
-#From build dir
-from="lgnecco@ssh.lamsade.dauphine.fr:/home/lamsade/lgnecco/stage/gedlib/compression/data/output/"
-to="/home/lucas/Documents/stage/gedlib/compression/data/from_server/"
-echo -ne '\007'
-eval "rsync -az -e ssh" $from $to
-echo -ne '\007'
-
-
